922acca929d54c368cd0aaa152db767dd52a9502
CyrilJl <joly.cyril7@gmail.com>
2023-08-12 16:39:33 +0200

    update build

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='optimask',
-      version='0.1',
+      version='0.1.1',
       packages=find_packages(),
       install_requires=['numpy', 'pandas'],
       long_description_content_type='text/markdown',
