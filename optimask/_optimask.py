2023e57d5f721239a5adfbc504e902eea652f2f4
CyrilJl <joly.cyril7@gmail.com>
2023-08-12 16:26:42 +0200

    update

--- optimask/_optimask.py
+++ optimask/_optimask.py
@@ -4,111 +4,74 @@
 
 
 from typing import Tuple, Union
+
 import numpy as np
 import pandas as pd
 
 
 class OptiMask:
     """
-    A class for optimizing data masking strategies based on NaN values.
+    This class is used to find the set of rows and columns to remove from a 2D array (numpy array or pandas DataFrame) with missing data, in order to obtain the resulting submatrix without any holes and as large as possible (i.e., with the maximum number of cells).
 
-    This class provides methods to optimize the arrangement of NaN values
-    in a given data array or DataFrame for efficient analysis or processing.
-    The optimization aims to remove rows and columns from the input matrix
-    to eliminate NaN values while maximizing the remaining data coverage.
+    The algorithm utilizes a method based on sorting rows and columns based on the number of missing values (NaN) they contain. Then, it searches for the optimal permutation of rows and columns to obtain the desired submatrix.
 
-    Attributes:
-        None
+    Reference: https://mathematica.stackexchange.com/a/284918/92680
 
-    Methods:
-        height(x: np.ndarray) -> int:
-            Calculate the height of the first non-NaN element in each column.
+    Example usage:
+    >>> from airpy.maths.optimask import OptiMask
+    >>> import numpy as np
 
-        permutation_columns(x: np.ndarray) -> np.ndarray:
-            Get the permutation indices of columns based on the number of NaN values.
+    >>> m, n = 120, 20
+    >>> data = np.zeros((m, n))
+    >>> data[24:72, 2] = np.nan
+    >>> data[0:24, 5] = np.nan
+    >>> data[100, :] = np.nan
+    >>> data[80:90, 7] = np.nan
 
-        permutation_rows(x: np.ndarray) -> np.ndarray:
-            Get the permutation indices of rows based on the number of NaN values.
+    >>> rows, cols = OptiMask()(data)
+    >>> subdata = OptiMask()(data, return_data=True)
 
-        _solve(x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-            Internal method to solve the optimization problem for a given data array.
-
-        solve(data: Union[np.ndarray, pd.DataFrame], return_data: bool = False) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
-            Solve the optimization problem for the provided data.
-
+    Note:
+        This class can be used to enhance model efficiency by removing missing data while retaining as much information as possible.
     """
 
-    @staticmethod
-    def height(x: np.ndarray) -> int:
-        """
-        Calculate the height of the first non-NaN element in each column.
-
-        Args:
-            x (np.ndarray): Input data array.
-
-        Returns:
-            int: The height of the first non-NaN element in each column.
-        """
-        non_zero_rows = np.nonzero(x)[0]
-        return non_zero_rows.max() + 1 if len(non_zero_rows) > 0 else 0
+    @classmethod
+    def height(cls, x: np.ndarray) -> int:
+        r = x.nonzero()[0]
+        if len(r) > 0:
+            return r.max() + 1
+        else:
+            return 0
 
     @staticmethod
     def permutation_columns(x: np.ndarray) -> np.ndarray:
-        """
-        Get the permutation indices of columns based on the number of NaN values.
-
-        Args:
-            x (np.ndarray): Input data array.
-
-        Returns:
-            np.ndarray: Permutation indices of columns based on the number of NaN values.
-        """
-        return np.isnan(x).sum(axis=0).argsort()[::-1]
-
-    @staticmethod
-    def permutation_rows(x: np.ndarray) -> np.ndarray:
-        """
-        Get the permutation indices of rows based on the number of NaN values.
-
-        Args:
-            x (np.ndarray): Input data array.
-
-        Returns:
-            np.ndarray: Permutation indices of rows based on the number of NaN values.
-        """
-        mask_nan = np.isnan(x)
-        sum_mask = mask_nan.sum(axis=1)
-
-        if np.any(sum_mask):
-            sorted_indices = np.argsort(sum_mask)[::-1]
-            non_zero_indices = np.nonzero(sum_mask)[0]
-            i0 = np.argmax(non_zero_indices >= 1) + 1
-            identity = np.arange(i0, len(mask_nan))
-            ret = np.concatenate([OptiMask.permutation_rows(
-                x[sorted_indices][:i0][:, 1:]), identity])
-            return sorted_indices[ret]
-        else:
-            return np.arange(mask_nan.shape[0])
+        return np.isnan(np.array(x)).sum(axis=0).argsort()[::-1]
 
     @classmethod
-    def _solve(cls, x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Internal method to solve the optimization problem for a given data array.
+    def permutation_rows(cls, x: np.ndarray) -> np.ndarray:
+        m = np.isnan(np.array(x))
+        if np.sum(m) > 0:
+            if x.shape[1] > 1:
+                p = m.sum(axis=1).argsort()[::-1]
+                i0 = np.argmax((m[p].sum(axis=1) > 0).nonzero()) + 1
+                identity = np.arange(i0, len(m))
+                ret = np.concatenate(
+                    [cls.permutation_rows(x[p][:i0][:, 1:]), identity])
+                return p[ret]
+            else:
+                return m.sum(axis=0).argsort()[::-1]
+        else:
+            return np.arange(m.shape[0])
 
-        Args:
-            x (np.ndarray): Input data array.
-
-        Returns:
-            Tuple[np.ndarray, np.ndarray]: Rows and columns indices for optimized arrangement.
-        """
+    @classmethod
+    def solve(cls, x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         m, n = x.shape
-        mask_nan = np.isnan(x)
-        p_cols = cls.permutation_columns(x)
-        p_rows = cls.permutation_rows(x)
+        mask_nan = np.isnan(np.array(x))
+        p_cols = cls.permutation_columns(np.array(x))
+        p_rows = cls.permutation_rows(np.array(x))
         processed_nan = mask_nan[p_rows][:, p_cols]
-
-        r = np.max(np.flip(np.maximum.accumulate(
-            np.flip([cls.height(x) for x in processed_nan.T] + [0]))[1:]))
+        r = [cls.height(x) for x in processed_nan.T] + [0]
+        r = np.flip([np.max(np.flip(r)[:k + 1]) for k in range(1, len(r))])
         r = np.array([[j, i, (n - i) * (m - j)] for i, j in enumerate(r)])
         k = np.argmax(r[:, 2])
         i, j = r[k, [0, 1]]
@@ -117,28 +80,17 @@ class OptiMask:
         return rows, cols
 
     @classmethod
-    def solve(cls, data: Union[np.ndarray, pd.DataFrame], return_data: bool = False) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
-        """
-        Solve the optimization problem for the provided data.
+    def __call__(cls, data: Union[np.ndarray, pd.DataFrame], return_data: bool = False) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
+        """Calls the solve method to find the rows and columns to remove and returns the results as indices or a submatrix.
 
-        Args:
-            data (Union[np.ndarray, pd.DataFrame]): Input data array or DataFrame.
-            return_data (bool, optional): Whether to return the optimized data. Defaults to False.
+        Parameters:
+            data (np.ndarray or pd.DataFrame): The 2D array with missing data.
+            return_data (bool, optional): Indicates whether the resulting submatrix should be returned. Default is False.
 
         Returns:
-            Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
-                If `return_data` is True:
-                    If `data` is a DataFrame, returns a tuple of row and column indices or slices for optimized arrangement.
-                    If `data` is a NumPy array, returns the optimized subset of the array.
-                If `return_data` is False:
-                    If `data` is a DataFrame, returns a tuple of row and column index objects for optimized arrangement.
-                    If `data` is a NumPy array, returns a tuple of row and column indices for optimized arrangement.
+            Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]: The indices of rows and columns to remove or the resulting submatrix, based on the value of return_data.
         """
-        if not isinstance(data, (np.ndarray, pd.DataFrame)):
-            raise ValueError(
-                "Input data must be a NumPy array or a Pandas DataFrame.")
-
-        rows, cols = cls._solve(data)
+        rows, cols = cls.solve(data)
 
         if isinstance(data, pd.DataFrame):
             if return_data:
