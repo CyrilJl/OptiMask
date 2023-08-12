f573c96220ed4121cfff24f71ca4d9a5bf69db1a
CyrilJl <joly.cyril7@gmail.com>
2023-08-12 16:40:53 +0200

    deleted:    dist/optimask-0.1.1-py3-none-any.whl 	deleted:    dist/optimask-0.1.1.tar.gz 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='optimask',
       packages=find_packages(),
       install_requires=['numpy', 'pandas'],
       long_description_content_type='text/markdown',
-      long_description="`OptiMask` is a cutting-edge Python package designed to optimize data masking strategies by intelligently arranging NaN values in a way that maximizes the data coverage, ensuring that the number of cells available for analysis is maximized."
+      long_description="README.md"
       )
