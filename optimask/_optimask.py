5c0f3682232c7a7e99ae669af7ecc866fb07da86
CyrilJl <joly.cyril7@gmail.com>
2023-08-15 17:39:53 +0200

    update

--- optimask/_optimask.py
+++ optimask/_optimask.py
@@ -10,87 +10,67 @@ import pandas as pd
 
 
 class OptiMask:
-    """
-    This class is used to find the set of rows and columns to remove from a 2D array (numpy array or pandas DataFrame) with missing data, in order to obtain the resulting submatrix without any holes and as large as possible (i.e., with the maximum number of cells).
-
-    The algorithm utilizes a method based on sorting rows and columns based on the number of missing values (NaN) they contain. Then, it searches for the optimal permutation of rows and columns to obtain the desired submatrix.
-
-    Reference: https://mathematica.stackexchange.com/a/284918/92680
-
-    Example usage:
-    >>> from airpy.maths.optimask import OptiMask
-    >>> import numpy as np
-
-    >>> m, n = 120, 20
-    >>> data = np.zeros((m, n))
-    >>> data[24:72, 2] = np.nan
-    >>> data[0:24, 5] = np.nan
-    >>> data[100, :] = np.nan
-    >>> data[80:90, 7] = np.nan
-
-    >>> rows, cols = OptiMask()(data)
-    >>> subdata = OptiMask()(data, return_data=True)
-
-    Note:
-        This class can be used to enhance model efficiency by removing missing data while retaining as much information as possible.
-    """
-
-    @classmethod
-    def height(cls, x: np.ndarray) -> int:
-        r = x.nonzero()[0]
-        if len(r) > 0:
-            return r.max() + 1
-        else:
-            return 0
+    """OptiMask class for finding the largest submatrix without NaN values."""
 
     @staticmethod
-    def permutation_columns(x: np.ndarray) -> np.ndarray:
-        return np.isnan(np.array(x)).sum(axis=0).argsort()[::-1]
+    def _sort_by_na_number(x: np.ndarray, axis: int = 0) -> np.ndarray:
+        return np.argsort(x.sum(axis=axis))[::-1]
+
+    @staticmethod
+    def _height(x: np.ndarray) -> int:
+        return x.nonzero()[0].max() + 1 if np.any(x) else 0
 
     @classmethod
-    def permutation_rows(cls, x: np.ndarray) -> np.ndarray:
-        m = np.isnan(np.array(x))
-        if np.sum(m) > 0:
-            if x.shape[1] > 1:
-                p = m.sum(axis=1).argsort()[::-1]
-                i0 = np.argmax((m[p].sum(axis=1) > 0).nonzero()) + 1
-                identity = np.arange(i0, len(m))
-                ret = np.concatenate(
-                    [cls.permutation_rows(x[p][:i0][:, 1:]), identity])
-                return p[ret]
-            else:
-                return m.sum(axis=0).argsort()[::-1]
-        else:
-            return np.arange(m.shape[0])
+    def _sort_by_na_max_index(cls, x: np.ndarray, axis: int = 0) -> np.ndarray:
+        if axis == 0:
+            return np.argsort([cls._height(p) for p in x.T])[::-1]
+        if axis == 1:
+            return np.argsort([cls._height(p) for p in x])[::-1]
+
+    @classmethod
+    def _get_largest_rectangle(cls, x: np.ndarray) -> Tuple[int, int]:
+        m, n = x.shape
+        heights = [cls._height(_) for _ in x.T]
+        areas = [(m - h) * (n - k) for k, h in enumerate(heights)]
+        i0 = np.argmax(areas)
+        return i0, heights[i0]
 
     @classmethod
     def _solve(cls, x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        m, n = x.shape
-        mask_nan = np.isnan(np.array(x))
-        p_cols = cls.permutation_columns(np.array(x))
-        p_rows = cls.permutation_rows(np.array(x))
-        processed_nan = mask_nan[p_rows][:, p_cols]
-        r = [cls.height(x) for x in processed_nan.T] + [0]
-        r = np.flip([np.max(np.flip(r)[:k + 1]) for k in range(1, len(r))])
-        r = np.array([[j, i, (n - i) * (m - j)] for i, j in enumerate(r)])
-        k = np.argmax(r[:, 2])
-        i, j = r[k, [0, 1]]
+        x = np.isnan(np.array(x))
+        p_cols_1 = cls._sort_by_na_number(x, axis=0)
+        p_rows_1 = cls._sort_by_na_number(x, axis=1)
+        x = x[p_rows_1][:, p_cols_1]
 
-        rows, cols = np.sort(p_rows[i:]), np.sort(p_cols[j:])
-        return rows, cols
+        p_cols_2 = cls._sort_by_na_max_index(x, axis=0)
+        p_rows_2 = cls._sort_by_na_max_index(x[:, p_cols_2], axis=1)
+
+        x = x[p_rows_2][:, p_cols_2]
+        i0, j0 = cls._get_largest_rectangle(x)
+        return np.sort(p_rows_1[p_rows_2][j0:]), np.sort(p_cols_1[p_cols_2][i0:])
 
     @classmethod
-    def solve(cls, data: Union[np.ndarray, pd.DataFrame], return_data: bool = False) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
-        """Calls the _solve method to find the rows and columns to remove and returns the results as indices or a submatrix.
+    def solve(
+        cls,
+        data: Union[np.ndarray, pd.DataFrame],
+        return_data: bool = False,
+    ) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
+        """
+        Finds the indices or submatrix of the largest non-NaN region in the input data.
 
-        Parameters:
-            data (np.ndarray or pd.DataFrame): The 2D array with missing data.
-            return_data (bool, optional): Indicates whether the resulting submatrix should be returned. Default is False.
+        Args:
+            data (Union[np.ndarray, pd.DataFrame]): Input data matrix or DataFrame.
+            return_data (bool, optional): If True, returns the submatrix instead of indices.
 
         Returns:
-            Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]: The indices of rows and columns to remove or the resulting submatrix, based on the value of return_data.
+            Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
+                If 'return_data' is False:
+                    - If 'data' is np.ndarray: Tuple of row indices and column indices of the submatrix.
+                    - If 'data' is pd.DataFrame: Tuple of row labels and column labels of the submatrix.
+                If 'return_data' is True:
+                    - If 'data' is np.ndarray: Submatrix of the largest non-NaN region.
+                    - If 'data' is pd.DataFrame: SubDataFrame of the largest non-NaN region.
         """
-        # Check if data is either a numpy array or a pandas DataFrame
         if not isinstance(data, (np.ndarray, pd.DataFrame)):
             raise ValueError("Input 'data' must be either a numpy array or a pandas DataFrame.")
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='optimask',
-      version='0.1.4',
+      version='0.1.6',
       packages=find_packages(),
       install_requires=['numpy', 'pandas'],
       long_description_content_type='text/markdown',
