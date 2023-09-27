e26dcec465c2b90cb8a0b162888479f2a4433fa2
CyrilJl <joly.cyril7@gmail.com>
2023-09-27 19:31:57 +0200

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='optimask',
-      version='0.1.7',
+      version='0.1.8',
       packages=find_packages(),
       install_requires=['numpy', 'pandas'],
       long_description_content_type='text/markdown',
