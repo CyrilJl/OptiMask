a835cbc6f8b9a3083d6525069659268af49b842e
CyrilJl <joly.cyril7@gmail.com>
2023-11-19 19:46:05 +0100

    major dev

--- optimask/_optimask.py
+++ optimask/_optimask.py
@@ -11,19 +11,41 @@ __all__ = ["OptiMask"]
 
 
 class OptiMask:
-    """OptiMask computes the largest (non-contiguous) submatrix without NaN values from the input matrix."""
-    MAX_STEPS = 32
+    """
+    OptiMask is a class for computing the optimal rows and columns to keep in a 2D array or DataFrame
+    in order to remove NaN values and retain the maximum amount of non-NaN cells. The class employs a heuristic
+    optimization approach, and increasing the value of `n_tries` generally results in more remaining cells,
+    potentially reaching or closely approaching the optimal amount.
+
+    Parameters:
+    - n_tries (int): The number of optimization attempts. Higher values may lead to better results.
+    - max_steps (int): The maximum number of steps to perform in each optimization attempt.
+    - random_state (Union[int, None]): Seed for the random number generator.
+
+    Attributes:
+    - n_tries (int): The number of optimization attempts.
+    - max_steps (int): The maximum number of steps to perform in each optimization attempt.
+    - random_state (Union[int, None]): Seed for the random number generator.
+    """
+
+    def __init__(self, n_tries=10, max_steps=32, random_state=None):
+        self.n_tries = n_tries
+        self.max_steps = max_steps
+        self.random_state = random_state
 
     @staticmethod
-    def _sort_by_na_number(x: np.ndarray, axis: int = 0) -> np.ndarray:
-        return np.argsort(x.sum(axis=axis))[::-1]
+    def _find_nan_indices(x):
+        nan_rows, nan_cols = np.nonzero(x)
+        nan_rows = np.unique(nan_rows)
+        nan_cols = np.unique(nan_cols)
+        return x[nan_rows][:, nan_cols], nan_rows, nan_cols
 
     @staticmethod
     def _height(x: np.ndarray) -> int:
         return x.nonzero()[0].max() + 1 if np.any(x) else 0
 
-    @classmethod
-    def _is_decreasing(cls, x):
+    @staticmethod
+    def _is_decreasing(x):
         return np.all(x[:-1] >= x[1:])
 
     @classmethod
@@ -34,24 +56,17 @@ class OptiMask:
             return np.argsort([cls._height(p) for p in x], kind='mergesort')[::-1]
 
     @classmethod
-    def _get_largest_rectangle(cls, x: np.ndarray) -> Tuple[int, int]:
-        m, n = x.shape
+    def _get_largest_rectangle(cls, x, m, n):
         heights = [cls._height(_) for _ in x.T]
         areas = [(m - h) * (n - k) for k, h in enumerate(heights)]
         i0 = np.argmax(areas)
-        return i0, heights[i0]
+        return heights[i0], i0, areas[i0]
 
     @classmethod
     def _is_pareto_ordered(cls, x):
         h0 = np.array([cls._height(_) for _ in x])
         h1 = np.array([cls._height(_) for _ in x.T])
         return cls._is_decreasing(h0) and cls._is_decreasing(h1)
-
-    @classmethod
-    def _process_init(cls, x):
-        p_cols = cls._sort_by_na_number(x, axis=0)
-        p_rows = cls._sort_by_na_number(x, axis=1)
-        return x[p_rows][:, p_cols], p_rows, p_cols
 
     @classmethod
     def _process_step(cls, x, axis):
@@ -61,96 +76,90 @@ class OptiMask:
         if axis == 1:
             return x[p], p
 
-    @classmethod
-    def _compute_permutations(cls, x):
-        xp, p_rows, p_cols = cls._process_init(x=np.isnan(np.array(x)))
+    @staticmethod
+    def _random_start(xp, rng):
+        p_rows = rng.permutation(xp.shape[0])
+        p_cols = rng.permutation(xp.shape[1])
+        return xp[p_rows][:, p_cols].copy(), p_rows, p_cols
 
+    def _compute_permutations(self, xpp, p_rows, p_cols):
         step = 0
-        while (not cls._is_pareto_ordered(xp)) and (step < cls.MAX_STEPS):
+        while (not self._is_pareto_ordered(xpp)) and (step < self.max_steps):
             axis = (step % 2)
             step += 1
-            xp, p_step = cls._process_step(xp, axis=axis)
+            xpp, p_step = self._process_step(xpp, axis=axis)
             if axis == 0:
                 p_cols = p_cols[p_step]
             if axis == 1:
                 p_rows = p_rows[p_step]
 
-        if not cls._is_pareto_ordered(xp):
+        if not self._is_pareto_ordered(xpp):
             raise ValueError("An error occurred during the computation of the optimal permutations.")
         else:
-            return xp, p_rows, p_cols
+            return xpp, p_rows, p_cols
 
-    @classmethod
-    def _solve(cls, x):
-        xp, p_rows, p_cols = cls._compute_permutations(x)
-        i0, j0 = cls._get_largest_rectangle(xp)
-        return np.sort(p_rows[j0:]), np.sort(p_cols[i0:])
+    def _trial(self, xp, m, n, rng):
+        xpp, p_rows, p_cols = self._random_start(xp=xp, rng=rng)
+        xpp, p_rows, p_cols = self._compute_permutations(xpp, p_rows, p_cols)
+        i0, j0, area = self._get_largest_rectangle(xpp, m, n)
+        return area, i0, j0, p_rows, p_cols
 
-    @classmethod
-    def solve(
-        cls,
-        data: Union[np.ndarray, pd.DataFrame],
-        return_data: bool = False,
-    ) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]:
+    def _solve(self, x):
+        m, n = x.shape
+        xp, nan_rows, nan_cols = self._find_nan_indices(x)
+        rng = np.random.default_rng(seed=self.random_state)
+
+        area_max = -1
+        for _ in range(self.n_tries):
+            area, i0, j0, p_rows, p_cols = self._trial(xp, m, n, rng)
+            if area > area_max:
+                area_max = area
+                opt = i0, j0, p_rows, p_cols
+
+        i0, j0, p_rows, p_cols = opt
+        rows_to_remove, cols_to_remove = nan_rows[p_rows[:i0]], nan_cols[p_cols[:j0]]
+
+        rows_to_keep = np.array([_ for _ in range(m) if _ not in rows_to_remove])
+        cols_to_keep = np.array([_ for _ in range(n) if _ not in cols_to_remove])
+        return rows_to_keep, cols_to_keep
+
+    def solve(self, X: Union[np.ndarray, pd.DataFrame], return_data: bool = False) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index]]:
         """
-        Solves the OptiMask problem to find the largest (non-contiguous) submatrix without NaN values in the input data.
+        Solve the optimal NaN removal problem for a 2D array or DataFrame.
 
-        Args:
-            data (Union[np.ndarray, pd.DataFrame]): The input data as a numpy array or a pandas DataFrame.
-            return_data (bool, optional): If True, returns the submatrix data. If False, returns row and column indices only. Default is False.
+        Parameters:
+        - X (Union[np.ndarray, pd.DataFrame]): The input 2D array or DataFrame with NaN values.
+        - return_data (bool): If True, return the resulting data; otherwise, return indices.
 
         Returns:
-            Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index], np.ndarray, pd.DataFrame]: A tuple containing the result based on the `return_data` parameter.
+        - Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index]]: If return_data is True,
+          returns the resulting 2D array or DataFrame; otherwise, returns the indices of rows and columns to keep.
 
-        Example:
-        ```
-        import numpy as np
-        from optimask import OptiMask
-
-        # Create a sample numpy array with NaN values
-        data = np.array([
-            [1, 2, np.nan, 4],
-            [np.nan, 6, np.nan, 8],
-            [9, 10, 11, 12],
-            [13, 14, np.nan, 16],
-        ])
-
-        # Instantiate the OptiMask class
-        optimask = OptiMask()
-
-        # Solve for the largest submatrix without NaN values
-        rows, cols = optimask.solve(data)
-
-        # Print the result
-        print("Row indices of the largest submatrix:", rows)
-        print("Column indices of the largest submatrix:", cols)
-
-        # Extract the largest submatrix from the original data
-        largest_submatrix = data[rows][:, cols]
-        print("Largest submatrix:")
-        print(largest_submatrix)
-        ```
+        Raises:
+        - ValueError: If the input data is not a numpy array or a pandas DataFrame,
+          or if the input numpy array does not have ndim==2,
+          or if the algorithm encounters an error during optimization.
         """
-        if not isinstance(data, (np.ndarray, pd.DataFrame)):
+        if not isinstance(X, (np.ndarray, pd.DataFrame)):
             raise ValueError("`data` must be a numpy array or a pandas DataFrame.")
 
         # Check dimensions for numpy array
-        if isinstance(data, np.ndarray) and data.ndim != 2:
-            raise ValueError("For a numpy array, 'data' must have ndim==2.")
+        if isinstance(X, np.ndarray) and X.ndim != 2:
+            raise ValueError("For a numpy array, 'X' must have ndim==2.")
 
-        rows, cols = cls._solve(data)
+        rows, cols = self._solve(np.isnan(np.asarray(X)))
 
-        if np.isnan(np.asarray(data)[rows][:, cols]).any():
+        if np.isnan(np.asarray(X)[rows][:, cols]).any():
             raise ValueError("The OptiMask algorithm encountered an error.")
 
-        if isinstance(data, pd.DataFrame):
+        if isinstance(X, pd.DataFrame):
             if return_data:
-                return data.iloc[rows, cols].copy()
+                return X.iloc[rows, cols].copy()
             else:
-                return data.index[rows], data.columns[cols]
+                return X.index[rows], data.columns[cols]
 
-        if isinstance(data, np.ndarray):
+        if isinstance(X, np.ndarray):
             if return_data:
-                return data[rows][:, cols].copy()
+                return X[rows][:, cols].copy()
             else:
                 return rows, cols
