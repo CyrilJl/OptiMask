be2ed8a18d76aa629e894f2f1463c000a1926348
CyrilJl <joly.cyril7@gmail.com>
2023-12-01 12:47:48 +0100

    coquille

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='optimask',
       version='0.1.12',
       packages=find_packages(),
       install_requires=['numpy', 'pandas'],
-      description="OptiMask: to extract the largest submatrix without NaN",
+      description="OptiMask: extracting the largest submatrix without NaN",
       long_description_content_type='text/markdown',
       long_description=long_description
       )
