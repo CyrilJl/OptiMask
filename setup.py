84b070e0c382d4c910133527926c1169f8df5753
CyrilJl <joly.cyril7@gmail.com>
2023-12-01 12:30:32 +0100

    add description

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='optimask',
       version='0.1.11',
       packages=find_packages(),
       install_requires=['numpy', 'pandas'],
+      description="OptiMask: to extract the largest submatrix without NaN",
       long_description_content_type='text/markdown',
       long_description=long_description
       )
