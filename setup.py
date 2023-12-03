7f201134561cbbdbf5d2230b03c45593b39033a2
CyrilJl <joly.cyril7@gmail.com>
2023-12-03 19:23:55 +0100

    version up + info

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='optimask',
-      version='0.1.13',
-      packages=find_packages(),
-      install_requires=['numpy', 'pandas'],
-      description="OptiMask: extracting the largest (non-contiguous) submatrix without NaN",
-      long_description_content_type='text/markdown',
-      long_description=long_description
-      )
+setup(
+    name='optimask',
+    version='0.1.14',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas'],
+    description="OptiMask: extracting the largest (non-contiguous) submatrix without NaN",
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    author='Cyril Joly',
+    license='MIT',
+    url='https://github.com/CyrilJl/optimask',
+    classifiers=['License :: OSI Approved :: MIT License'],
+)
