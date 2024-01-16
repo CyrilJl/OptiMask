3025f0f9de6ee1841bd53f7ee36b1a8f22947e37
CyrilJl <joly.cyril7@gmail.com>
2024-01-16 20:35:54 +0100

    major speed improvement

--- optimask/_optimask.py
+++ optimask/_optimask.py
@@ -39,15 +39,8 @@ class OptiMask:
             print(msg)
 
     @staticmethod
-    def _find_nan_indices(x):
-        nan_rows, nan_cols = np.nonzero(x)
-        nan_rows = np.unique(nan_rows)
-        nan_cols = np.unique(nan_cols)
-        return x[nan_rows][:, nan_cols], nan_rows, nan_cols
-
-    @classmethod
-    def _heights(cls, x, axis=0):
-        return x.shape[axis] - np.argmax(np.flip(x, axis=axis), axis=axis)
+    def _heights(df, axis=0):
+        return df.groupby(axis)[1-axis].max().values + 1
 
     @staticmethod
     def _is_decreasing(x) -> bool:
@@ -61,75 +54,70 @@ class OptiMask:
     def _get_largest_rectangle(cls, heights, m, n):
         areas = (m - heights) * (n - np.arange(len(heights)))
         i0 = np.argmax(areas)
-        return heights[i0], i0, areas[i0]
+        return i0, heights[i0], areas[i0]
+
+    @staticmethod
+    def _permutation_index(p):
+        inv = np.empty_like(p)
+        inv[p] = np.arange(len(inv), dtype=inv.dtype)
+        return inv
 
     @classmethod
     def _is_pareto_ordered(cls, hx, hy):
         return cls._is_decreasing(hx) and cls._is_decreasing(hy)
 
-    @classmethod
-    def _process_step(cls, x, height, axis):
-        p = cls._sort_by_na_max_index(height)
-        if axis == 0:
-            return x[:, p], p
-        if axis == 1:
-            return x[p], p
+    def _trial(self, rng, df, m, n, m_nan, n_nan):
+        p_rows = rng.permutation(m_nan)
+        p_cols = rng.permutation(n_nan)
 
-    @staticmethod
-    def _random_start(xp, rng):
-        p_rows = rng.permutation(xp.shape[0])
-        p_cols = rng.permutation(xp.shape[1])
-        return xp[p_rows][:, p_cols], p_rows, p_cols
+        df_trial = pd.DataFrame()
+        df_trial[0] = self._permutation_index(p_rows)[df[0].values]
+        df_trial[1] = self._permutation_index(p_cols)[df[1].values]
 
-    def _compute_permutations(self, xpp, p_rows, p_cols):
         step = 0
-        heights = self._heights(xpp, axis=0), self._heights(xpp, axis=1)
+        heights = self._heights(df_trial, axis=0), self._heights(df_trial, axis=1)
         while (not self._is_pareto_ordered(*heights)) and (step < self.max_steps):
             axis = (step % 2)
             step += 1
-            xpp, p_step = self._process_step(xpp, heights[axis], axis=axis)
+            p_step = self._sort_by_na_max_index(heights[axis])
+            df_trial[axis] = self._permutation_index(p_step)[df_trial[axis].values]
             if axis == 0:
-                p_cols = p_cols[p_step]
-                heights = (heights[0][p_step], self._heights(xpp, axis=1))
-            if axis == 1:
                 p_rows = p_rows[p_step]
-                heights = (self._heights(xpp, axis=0), heights[1][p_step])
+                heights = (heights[0][p_step], self._heights(df_trial, axis=1))
+            if axis == 1:
+                p_cols = p_cols[p_step]
+                heights = (self._heights(df_trial, axis=0), heights[1][p_step])
 
         if not self._is_pareto_ordered(*heights):
             raise ValueError("An error occurred while calculating optimal permutations. "
                              "You can try again with a larger `max_steps` value.")
         else:
-            return xpp, p_rows, p_cols, heights
-
-    def _trial(self, xp, m, n, rng):
-        xpp, p_rows, p_cols = self._random_start(xp=xp, rng=rng)
-        xpp, p_rows, p_cols, heights = self._compute_permutations(xpp, p_rows, p_cols)
-        i0, j0, area = self._get_largest_rectangle(heights[0], m, n)
-        return area, i0, j0, p_rows, p_cols
+            i0, j0, area = self._get_largest_rectangle(heights[1], m, n)
+            return area, i0, j0, p_rows, p_cols
 
     def _solve(self, x):
         m, n = x.shape
-        xp, nan_rows, nan_cols = self._find_nan_indices(x)
-        self._verbose(f"{len(nan_rows)} rows and {len(nan_cols)} columns contain NaNs.")
+        df = pd.DataFrame()
+        iy, ix = np.isnan(x).nonzero()
+        rows_with_nan, df[0] = np.unique(iy, return_inverse=True)
+        cols_with_nan, df[1] = np.unique(ix, return_inverse=True)
+        m_nan, n_nan = len(rows_with_nan), len(cols_with_nan)
 
-        if len(nan_rows) == 0:
-            return np.arange(m), np.arange(n)
-        else:
-            rng = np.random.default_rng(seed=self.random_state)
-            area_max = -1
-            for k in range(self.n_tries):
-                area, i0, j0, p_rows, p_cols = self._trial(xp, m, n, rng)
-                self._verbose(f"\tTrial {k+1} : submatrix of size {m-i0}x{n-j0} ({area} elements) found.")
-                if area > area_max:
-                    area_max = area
-                    opt = i0, j0, p_rows, p_cols
+        rng = np.random.default_rng(seed=self.random_state)
+        area_max = -1
+        for k in range(self.n_tries):
+            area, i0, j0, p_rows, p_cols = self._trial(rng, df, m, n, m_nan, n_nan)
+            self._verbose(f"\tTrial {k+1} : submatrix of size {m-j0}x{n-i0} ({area} elements) found.")
+            if area > area_max:
+                area_max = area
+                opt = i0, j0, p_rows, p_cols
 
-            i0, j0, p_rows, p_cols = opt
-            self._verbose(f"Result: the largest submatrix found is of size {m-i0}x{n-j0} ({area_max} elements) found.")
+        i0, j0, p_rows, p_cols = opt
+        self._verbose(f"Result: the largest submatrix found is of size {m-j0}x{n-i0} ({area_max} elements) found.")
 
-            rows_to_keep = np.setdiff1d(np.arange(m), nan_rows[p_rows[:i0]])
-            cols_to_keep = np.setdiff1d(np.arange(n), nan_cols[p_cols[:j0]])
-            return rows_to_keep, cols_to_keep
+        rows_to_keep = np.setdiff1d(np.arange(m), rows_with_nan[p_rows[:j0]])
+        cols_to_keep = np.setdiff1d(np.arange(n), cols_with_nan[p_cols[:i0]])
+        return rows_to_keep, cols_to_keep
 
     def solve(self, X: Union[np.ndarray, pd.DataFrame], return_data: bool = False) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[pd.Index, pd.Index]]:
         """
@@ -154,7 +142,7 @@ class OptiMask:
         if isinstance(X, np.ndarray) and X.ndim != 2:
             raise ValueError("For a numpy array, 'X' must have ndim==2.")
 
-        rows, cols = self._solve(np.isnan(np.asarray(X)))
+        rows, cols = self._solve(np.asarray(X))
 
         if np.isnan(np.asarray(X)[rows][:, cols]).any():
             raise ValueError("The OptiMask algorithm encountered an error.")
