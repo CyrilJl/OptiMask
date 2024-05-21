b9970b8d24940e128ea5f063480847508ee2ecac
Cyril Joly <joly.cyril7@gmail.com>
2024-05-21 12:23:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='optimask',
-      version='1.1.2.0',
+      version='1.1.2.1',
       packages=find_packages(),
       install_requires=['numpy', 'pandas'],
       description="OptiMask: extracting the largest (non-contiguous) submatrix without NaN",
