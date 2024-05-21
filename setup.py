f1101c2749a5e4d7f830b1101ef89de04cb9a521
Cyril Joly <joly.cyril7@gmail.com>
2024-05-21 13:17:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 import numpy as np
+from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='optimask',
-      version='1.1.2.1',
-      packages=find_packages(),
-      install_requires=['numpy', 'pandas'],
-      description="OptiMask: extracting the largest (non-contiguous) submatrix without NaN",
-      long_description_content_type='text/markdown',
-      long_description=long_description,
-      author='Cyril Joly',
-      license='MIT',
-      url='https://github.com/CyrilJl/optimask',
-      classifiers=['License :: OSI Approved :: MIT License'],
-      ext_modules=[Extension("optimask.optimask_cython",
-                             sources=["optimask/optimask_cython.pyx"],
-                             include_dirs=[np.get_include()])]
-      )
+setup(
+    name='optimask',
+    version='1.1.1',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'cython'],
+    description="OptiMask: extracting the largest (non-contiguous) submatrix without NaN",
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    author='Cyril Joly',
+    license='MIT',
+    url='https://github.com/CyrilJl/optimask',
+    classifiers=['License :: OSI Approved :: MIT License'],
+    ext_modules=cythonize([
+        Extension(
+            "optimask.optimask_cython",
+            sources=["optimask/optimask_cython.pyx"],
+            include_dirs=[np.get_include()],
+            extra_compile_args=['-std=c99'],
+            extra_link_args=['-std=c99']
+        )
+    ])
+)
