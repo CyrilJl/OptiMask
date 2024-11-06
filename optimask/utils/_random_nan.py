6727a00529ea21c8076b8ea9bf5cd87b2d587871
CyrilJl <joly.cyril7@gmail.com>
2024-11-06 10:46:29 +0100

    utils module

--- optimask/utils/_random_nan.py
+++ optimask/utils/_random_nan.py
@@ -3,5 +3,6 @@ import numpy as np
 
 def generate_mar(m, n, ratio) -> np.ndarray:
     """Missing at random 2D array"""
-    assert 0 <= ratio <= 1, "Ratio must be a float between 0 and 1."
+    if ratio < 0 or 1 < ratio:
+        raise ValueError("Ratio must be a float between 0 and 1.")
     return np.random.choice(a=[0, np.nan], size=(m, n), p=[1-ratio, ratio])
