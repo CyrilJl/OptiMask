d24930535c96fbb3a7bcc967159375706c319637
CyrilJl <joly.cyril7@gmail.com>
2025-03-02 12:04:24 +0100

    update test cases for OptiMask: change array dtype to float32, add full NaNs test, and enhance seed testing

--- tests/test_optimask.py
+++ tests/test_optimask.py
@@ -8,7 +8,7 @@ from optimask import OptiMask
 
 def generate_random(m, n, ratio):
     """Missing at random arrays"""
-    arr = np.zeros((m, n))
+    arr = np.zeros((m, n), dtype=np.float32)
     nan_count = int(ratio * m * n)
     indices = np.random.choice(m * n, nan_count, replace=False)
     arr.flat[indices] = np.nan
@@ -16,7 +16,7 @@ def generate_random(m, n, ratio):
 
 
 @pytest.fixture
-def opti_mask_instance():
+def opti_mask_instance() -> OptiMask:
     return OptiMask(verbose=1)
 
 
@@ -69,6 +69,13 @@ def test_no_nan(opti_mask_instance):
     assert np.allclose(x, opti_mask_instance.solve(x, return_data=True))
 
 
+def test_full_nans(opti_mask_instance):
+    x = np.full(shape=(500, 1000), fill_value=np.nan)
+    r, c = opti_mask_instance.solve(x)
+    assert len(r) == 0
+    assert len(c) == 1000
+
+
 def test_one_col(opti_mask_instance):
     m = 100
     x = np.arange(m, dtype=float)[:, None]
@@ -97,3 +104,20 @@ def test_nan_in_one_row(opti_mask_instance):
         x = np.zeros(shape=(m, n))
         x[0, :25] = np.nan
         opti_mask_instance.solve(X=x, check_result=True)
+
+
+def test_seed():
+    """The test between results of two different seeds can be subject to
+    collisions, but it is unlikely.
+    """
+    X = generate_random(m=10_000, n=500, ratio=0.025)
+    om1 = OptiMask(random_state=99)
+    om2 = OptiMask(random_state=99)
+    om3 = OptiMask(random_state=999)
+    rows1, cols1 = om1.solve(X, check_result=True)
+    rows2, cols2 = om2.solve(X, check_result=True)
+    rows3, cols3 = om3.solve(X, check_result=True)
+    assert np.allclose(rows1, rows2)
+    assert np.allclose(cols1, cols2)
+    assert (len(rows1) != len(rows3)) or (not np.allclose(rows1, rows3))
+    assert (len(cols1) != len(cols3)) or (not np.allclose(cols1, cols3))
