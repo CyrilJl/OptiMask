182a701a8531c53601427ad0a54a756c349e7cc2
Cyril <joly.cyril7@gmail.com>
2025-03-13 14:58:46 +0100

    empty instead of ones

--- optimask/_optimask.py
+++ optimask/_optimask.py
@@ -142,7 +142,7 @@ class OptiMask:
         m, n = x.shape
         iy, ix = np.empty(m * n, dtype=np.uint32), np.empty(m * n, dtype=np.uint32)
         cols_index_mapper = -np.ones(n, dtype=np.int16)
-        rows_with_nan = np.zeros(m, dtype=np.uint32)
+        rows_with_nan = np.empty(m, dtype=np.uint32)
         n_rows_with_nan = 0
         n_cols_with_nan = 0
         cnt = 0
