ef916259193ffbd3532337b3917c85977f79c24a
CyrilJl <joly.cyril7@gmail.com>
2025-07-04 11:24:15 +0200

    Refactor generate_mar function to use a random number generator and update docstring

--- optimask/utils/_random_nan.py
+++ optimask/utils/_random_nan.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def generate_mar(m, n, ratio) -> np.ndarray:
-    """Missing at random 2D array"""
+def generate_mar(m, n, ratio, rng=None) -> np.ndarray:
+    """Missing at random 2D array, filled with 0s and NaNs"""
+    rng = np.random.default_rng(rng)
     if ratio < 0 or 1 < ratio:
         raise ValueError("Ratio must be a float between 0 and 1.")
-    return np.random.choice(a=[0, np.nan], size=(m, n), p=[1 - ratio, ratio])
+    return rng.choice(a=[0, np.nan], size=(m, n), p=[1 - ratio, ratio])
